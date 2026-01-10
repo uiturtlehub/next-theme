31230e65372856e8affbe2ad3ee33c4148d160e0
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-10 11:01:05 +0800

    Added dark classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-950 text-white">
+    <div className="flex h-screen w-screen items-center justify-center bg-neutral-100 text-neutral-800 dark:bg-neutral-800 dark:text-neutral-100">
       <div className="text-center">
         <h1 className="text-2xl font-bold">Next.js + Tailwind CSS Theme</h1>
       </div>
