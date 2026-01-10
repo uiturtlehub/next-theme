6b85c2211f00444f71989b25597069ee1d6c2e96
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-10 11:07:01 +0800

    Added semicolon

--- lib/button.tsx
+++ lib/button.tsx
@@ -10,5 +10,5 @@ export default function Button() {
         onClick={handleChangeTheme}
         className="px-6 py-2 rounded-md bg-green-600 text-white font-semibold cursor-pointer mt-4 hover:bg-green-500 transition duration-300"
         >Change Theme</button>
-    )
+    );
 }
