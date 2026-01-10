18ce751a274b68e503d33f47ce5312cbf5fc7ca0
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-10 11:16:25 +0800

    Added next theme provider and functions

--- lib/button.tsx
+++ lib/button.tsx
@@ -1,14 +1,26 @@
 'use client'
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 export default function Button() {
 
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState<boolean>(false);
+
+    useEffect(() => setMounted(true), []);
+    if (!mounted) return null;
+
     const handleChangeTheme = () => {
-        alert("Theme updated!");
+        setTheme(theme === "dark" ? "light" : "dark");
     }
 
     return (
         <button
         onClick={handleChangeTheme}
         className="px-6 py-2 rounded-md bg-green-600 text-white font-semibold cursor-pointer mt-4 hover:bg-green-500 transition duration-300"
-        >Change Theme</button>
+        >
+            {theme === "dark" ? "Switch to Light" : "Switch to Dark"}
+        </button>
     );
 }
