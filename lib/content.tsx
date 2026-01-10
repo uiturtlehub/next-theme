22b569bddff303f7d58b1dd5604427135acb31f7
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-10 11:20:20 +0800

    Updated content to client side component

--- lib/content.tsx
+++ lib/content.tsx
@@ -1,6 +1,17 @@
+'use client'
+
+import { useTheme } from "next-themes";
 import Button from "./button";
+import { useEffect, useState } from "react";
 
 export default function Content() {
+
+    const { theme } = useTheme();
+    const [mounted, setMounted] = useState<boolean>(false);
+
+    useEffect(() => setMounted(true), []);
+    if(!mounted) return null;
+
     return (
         <div className="flex h-screen w-screen items-center justify-center bg-neutral-100 text-neutral-800 dark:bg-neutral-800 dark:text-neutral-100">
             <div className="text-center">
