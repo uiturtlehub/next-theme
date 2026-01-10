fb722ffa5cedbaa4aea8aaa04a39ce842d508cfe
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-10 11:05:36 +0800

    Added them toggle button

---
+++ lib/button.tsx
@@ -0,0 +1,14 @@
+'use client'
+export default function Button() {
+
+    const handleChangeTheme = () => {
+        alert("Theme updated!");
+    }
+
+    return (
+        <button
+        onClick={handleChangeTheme}
+        className="px-6 py-2 rounded-md bg-green-600 text-white font-semibold cursor-pointer mt-4 hover:bg-green-500 transition duration-300"
+        >Change Theme</button>
+    )
+}
