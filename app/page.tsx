1024c3b06a4ae60a42a1932401d53451cd234ff0
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-10 11:06:55 +0800

    Moduralized content

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Button from "@/lib/button";
+import Content from "@/lib/content";
 
 export default function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-neutral-100 text-neutral-800 dark:bg-neutral-800 dark:text-neutral-100">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold">Next.js + Tailwind CSS Theme</h1>
-        <Button/>
-      </div>
-    </div>
-  )
+    <Content/>
+  );
 }
